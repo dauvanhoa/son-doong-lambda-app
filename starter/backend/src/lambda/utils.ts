bd9127dc62fef8336d9ebc7962d73bb8dbc0a3a8
dauvanhoa <dauvanhoa.it@gmail.com>
2023-10-15 18:27:29 +0700

    Fix get user authen

--- starter/backend/src/lambda/utils.ts
+++ starter/backend/src/lambda/utils.ts
@@ -1,9 +1,20 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import { parseUserId } from '../auth/utils'
 
-export function getUserId(event) {
-  const authorization = event.headers.Authorization
-  const split = authorization.split(' ')
-  const jwtToken = split[1]
+/**
+ * Get a user id from a JWT token
+ * @param event API Gateway event
+ * @returns a user id from a JWT token
+ */
+export function getUserId(event: APIGatewayProxyEvent) {
+  // TODO: Return a user id from the JWT token
+  // Get Authorization header from event
+  const authorization = event.headers.Authorization;
 
-  return parseUserId(jwtToken)
+  // Get the token from the Authorization header
+  const split = authorization.split(" ");
+  const jwtToken = split[1];
+
+  // Parse the JWT token and return a user id
+  return parseUserId(jwtToken);
 }
